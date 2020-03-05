6fdf3293653e6c132538f92c3530935ebf8a3535
wisnu-antara <wisnu.4nt4ra@gmail.com>
2020-03-05 15:05:43 +0800

    chellenge part 1

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const electron = require("electron");
 const {
     app,
     BrowserWindow,
-    MEnu,
+    Menu,
     ipcMain
 } = electron;
 
 let todayWindow;
 let createWindow;
 let listWindow;
+let aboutWindow;
 
 app.on("ready", () => {
     todayWindow = new BrowserWindow({
@@ -24,4 +25,93 @@ app.on("ready", () => {
             app.quit();
             todayWindow = null;
     });
+const mainMenu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`)
+    listWindow.on("closed", () => (listWindow = null));
+};
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`)
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "About"
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/about.html`)
+    aboutWindow.on("closed", () => (aboutWindow = null));
+};
+
+    ipcMain.on("appointment:create", (event, appointment) => {
+    console.log(appointment);
+});
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "Menu Appoinment",
+
+        click(){
+            createWindowCreator();
+        }
+    },
+    {
+        label: "All Appointment",
+        click() {
+            listWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command+Q" :
+        "Ctrl + Q",
+        click() {
+            app.quit();
+        }
+    }
+    ]
+}, 
+
+{
+    label: "View",
+    submenu: [{ role: "reload"}, { role: "toggledevtools"}]
+},
+{
+    label: "About",
+    click() {
+        aboutWindowCreator();
+    }
+}
+]
