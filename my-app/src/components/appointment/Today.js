747d0acf4f5ff7b0814170673d884bf52f5983aa
wisnu-antara <wisnu.4nt4ra@gmail.com>
2020-06-19 22:45:54 +0800

    part2

---
+++ my-app/src/components/appointment/Today.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Today extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Ini Create</h3>
+            </div>
+        )
+    }
+}
+
+export default Today
